b438cfeaf96a4b75f045f94b0d38d81b66b51962
Robert Knezevic <robertknezevic77@gmail.com>
2025-05-21 09:47:04 +0200

    improved code

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,12 @@ import connectDb from './DatabaseLogic/dbConfig';
 import {config} from './config/config';
 import { errorHandler } from './middleware/errorHandler';
 import router from './Routes/router';
+import fetchAndSaveNewsJob from './jobs/cronJobs';
 
 const app: Application = express();
 app.use(express.json());
-
 app.use(router);
-
+fetchAndSaveNewsJob;
 app.use(errorHandler)
 connectDb();
 
