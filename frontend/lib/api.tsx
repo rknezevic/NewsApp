3bdf7270ebf50da8804e35427844c2531109f394
Robert Knezevic <robertknezevic77@gmail.com>
2025-06-02 13:07:50 +0200

    project setup

---
+++ frontend/lib/api.tsx
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+export default api;
