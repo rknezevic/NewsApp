99fb075d7e7c47d6a533ff1ca5fc4e57ecc3492f
Robert Knezevic <robertknezevic77@gmail.com>
2025-06-25 12:38:09 +0200

    fixed published by to display correctly and exported function for parsing date

--- frontend/src/components/DetailsView/NewsPostDetails.tsx
+++ frontend/src/components/DetailsView/NewsPostDetails.tsx
@@ -3,6 +3,7 @@
 import { fetchNewsPostDetails } from "@/features/actions/newsPostDetails";
 import { useQuery } from "@tanstack/react-query";
 import styles from './NewsPostDetails.module.css';
+import { PublisherType } from "@/enums/PublisherType";
 
 export default function NewsPostDetails({ postId }: { postId: string }) {
     const { data, isLoading, isError } = useQuery({
@@ -13,13 +14,17 @@ export default function NewsPostDetails({ postId }: { postId: string }) {
     if (isLoading) return <p>Loading post details...</p>;
     if (isError) return <p>Error loading post details: {isError}</p>;
 
+    const parsedDate = (date: string) => {
+        return new Date(date).toLocaleDateString();
+    };
+
     return (
     <div className={styles.container}>
       <div className={styles.headerRow}>
         <h1 className={styles.headline}>{data.headline}</h1>
         <div className={styles.meta}>
-          <p>Published by {data.createdBy.name} || 'Guest'</p>
-          <p>{new Date(data.createdAt).toLocaleDateString()}</p>
+          <p>Published by {data.createdBy.name || PublisherType.Guest}</p>
+          <p>{parsedDate(data.createdAt)}</p>
         </div>
       </div>
 
