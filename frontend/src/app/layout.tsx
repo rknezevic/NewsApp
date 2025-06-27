69ed2b987e057300f5fb82e8d29f07b1e16a4b0d
Robert Knezevic <robertknezevic77@gmail.com>
2025-06-27 10:27:20 +0200

    deleted additional clientLayout function for hiding the headers from auth routes and implemented it directly in the layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,18 +1,23 @@
+'use client';
 import "./globals.css";
 import QueryProvider from "../features/QueryProvider";
-import ClientLayout from "./clientLayout";
-const noHeader = ["/signin", "signup"]
-export default function RootLayout({
+import { Header } from "@/components/Header/Header";
+import { use } from "react";
+import { usePathname } from "next/navigation";
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-   return (
+  const pathname = usePathname();
+  const noHeader = ['/signin', '/signup'];
+  return (
     <html lang="en">
       <body>
         <QueryProvider>
           <main>
-            <ClientLayout>{children}</ClientLayout>
+            {!noHeader.includes(pathname) && <Header />}
+            {children}
           </main>
         </QueryProvider>
       </body>
